9425902f49afc9d56912b06c6431b8810e586785
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2025-10-08 14:14:59 +0100

    redirect

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='admin/', permanent=True)),
     path('admin/', admin.site.urls),
     path('api/account/', include("users.urls")),
     path('api/wallet/', include("wallet.urls")),
